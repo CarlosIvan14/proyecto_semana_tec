7c141f94ad516ccb46caaef2d08083c63b66e9a0
Carlos Iván Armenta Naranjo <110949034+CarlosIvan14@users.noreply.github.com>
2023-10-24 15:31:12 -0600

    Add files via upload

--- semana_tec/src/App.js
+++ semana_tec/src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import React, { useState } from 'react';
+import CustomNavbar from './components/UserNavbar';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <div className="App">
-      
+      <CustomNavbar loggedIn={setLoggedIn} />
+      {/* Resto de tu contenido */}
     </div>
   );
 }
 
 export default App;
+
